1eafd0c69210e125bd951c720aacf5d57f981d3d
dtsiamasiotis <dtsiamasiotis@gmail.com>
2018-09-10 02:48:57 +0300

    find current position with geolocation

--- app.js
+++ app.js
@@ -2,23 +2,63 @@
     'use strict';
 
     angular.module('TruckMonitorClient',[])
-        .controller('TruckMonitorClientController',TruckMonitorClientController);
+        .controller('TruckMonitorClientController',TruckMonitorClientController)
+        .service('GeolocationService',GeolocationService);
 
-    TruckMonitorClientController.$inject = ['$scope'];
+    TruckMonitorClientController.$inject = ['GeolocationService','$timeout'];
 
-    function TruckMonitorClientController($scope){
+    function TruckMonitorClientController(GeolocationService,$timeout){
+        this.socket = new WebSocket("ws://localhost:8080/servlet/actions");
+        this.lat = 0;
+        this.lng = 0;
         this.sendMessage = function(message){
-            var socket = this.connectToServer();
-           socket.onopen = function(event)
-           {
-               socket.send(message);
+
                console.log(message);
-           }
+               this.socket.send(message);
 
         };
-        this.connectToServer = function(){
-            var socket = new WebSocket("ws://localhost:8080/servlet/actions");
-            return socket;
+
+
+
+        this.socket.onmessage = function () {
+            console.log("message received");
+            GeolocationService.getLocation();
+            $timeout(function (socket){
+                socket.send("coordinates:"+GeolocationService.getLat()+","+GeolocationService.getLng());
+            },5000,true,this);
+
+        };
+
+
+        this.socketIsOpen = function(){
+            return (this.socket.readyState===1);
         }
         };
+
+    function GeolocationService(){
+        var service = this;
+        var lat,lng;
+
+        service.getLocation = function() {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(service.showPosition);
+            } else {
+                alert("Geolocation is not supported by this browser.");
+            }
+        };
+
+        service.showPosition=function(position) {
+            lat = position.coords.latitude;
+            lng = position.coords.longitude;
+           console.log(lat,lng);
+        };
+
+        service.getLat = function(){
+            return lat;
+        }
+
+        service.getLng = function(){
+            return lng;
+        }
+    }
 })();
